b0b4e75cd26e85dd1554954e28df430850b66414
Halysson <halysson1007@gmail.com>
2025-12-19 17:04:21 -0300

    here go

---
+++ hello_world/gen/obj/hello_world.d
@@ -0,0 +1,39 @@
+gen/obj/hello_world.o: hello_world.c ../nuttx-export/include/stdio.h \
+ ../nuttx-export/include/nuttx/config.h \
+ ../nuttx-export/include/sys/types.h \
+ ../nuttx-export/include/nuttx/compiler.h \
+ ../nuttx-export/include/stdint.h ../nuttx-export/include/arch/types.h \
+ ../nuttx-export/include/arch/inttypes.h ../nuttx-export/include/limits.h \
+ ../nuttx-export/include/arch/limits.h ../nuttx-export/include/time.h \
+ ../nuttx-export/include/nuttx/fs/fs.h \
+ ../nuttx-export/include/nuttx/fs/ioctl.h \
+ ../nuttx-export/include/stdbool.h \
+ ../nuttx-export/include/nuttx/serial/tioctl.h \
+ ../nuttx-export/include/nuttx/fs/uio.h ../nuttx-export/include/sys/uio.h \
+ ../nuttx-export/include/dirent.h ../nuttx-export/include/nuttx/atomic.h \
+ ../nuttx-export/include/nuttx/mutex.h ../nuttx-export/include/assert.h \
+ ../nuttx-export/include/nuttx/semaphore.h \
+ ../nuttx-export/include/errno.h ../nuttx-export/include/semaphore.h \
+ ../nuttx-export/include/nuttx/queue.h \
+ ../nuttx-export/include/nuttx/clock.h \
+ ../nuttx-export/include/nuttx/lib/math32.h \
+ ../nuttx-export/include/inttypes.h ../nuttx-export/include/stddef.h \
+ ../nuttx-export/include/strings.h ../nuttx-export/include/string.h \
+ ../nuttx-export/include/alloca.h ../nuttx-export/include/nuttx/mm/map.h \
+ ../nuttx-export/include/nuttx/mm/gran.h \
+ ../nuttx-export/include/nuttx/spawn.h ../nuttx-export/include/spawn.h \
+ ../nuttx-export/include/sched.h ../nuttx-export/include/signal.h \
+ ../nuttx-export/include/nuttx/irq.h ../nuttx-export/include/arch/irq.h \
+ ../nuttx-export/include/arch/syscall.h \
+ ../nuttx-export/include/arch/chip/irq.h \
+ ../nuttx-export/include/arch/stm32h7/stm32h7x3xx_irq.h \
+ ../nuttx-export/include/arch/armv7-m/irq.h \
+ ../nuttx-export/include/arch/armv7-m/nvicpri.h \
+ ../nuttx-export/include/arch/chip/chip.h \
+ ../nuttx-export/include/nuttx/spinlock_type.h \
+ ../nuttx-export/include/nuttx/lib/lib.h \
+ ../nuttx-export/include/nuttx/kmalloc.h ../nuttx-export/include/stdlib.h \
+ ../nuttx-export/include/nuttx/mm/mm.h \
+ ../nuttx-export/include/nuttx/userspace.h \
+ ../nuttx-export/include/pthread.h ../nuttx-export/include/unistd.h \
+ ../nuttx-export/include/malloc.h
---
+++ hello_world/hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int print_msg (void)
+{
+	int a = 2, b = 10;
+	printf("%d\n", a+b);
+	return (a+b);
+}
